5eed2fc9c10e755e400359847ac62d07e3f46f09
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-07-02 23:59:33 +0200

    chore: daisyui + small fixes to i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,23 +1,19 @@
 export const locales = {
-	sr: "Srpski",
-	"sr-Cyrl": "Српски",
-	en: "English",
-};
-
-export const defaultLocale = "sr";
-
-export const localeTags = {
 	sr: "sr-Latn-RS",
 	"sr-Cyrl": "sr-Cyrl-RS",
 	en: "en",
 };
 
+export const defaultLocale = "sr";
+
 export const ui = {
 	sr: {},
 	"sr-Cyrl": {
+		"locale.short": "СР",
 		"site.title": "РУР Роботика",
 	},
 	en: {
+		"locale.short": "EN",
 		"site.title": "RUR Robotics",
 	},
 } as const;
