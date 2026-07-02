4160ba33f64fcf4356689666850c548ce31de156
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-07-02 22:28:39 +0200

    feat: add i18n with transliteration

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const locales = {
+	sr: "Srpski",
+	"sr-Cyrl": "Српски",
+	en: "English",
+};
+
+export const defaultLocale = "sr";
+
+export const ui = {
+	sr: {
+	},
+	"sr-Cyrl": {
+		"site.title": "РУР Роботика",
+	},
+	en: {
+		"site.title": "RUR Robotics",
+	},
+} as const;
