f59b6eacdeb9fd887e1ebbcc15e7b02c44e9862f
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-07-13 20:48:40 +0200

    chore: replace daisyui with svelte-shadcn

---
+++ src/locales/data.js
@@ -0,0 +1,3 @@
+/** @typedef {('en'|'sr-Cyrl'|'sr')} Locale */
+/** @type {[Locale, ...Locale[]]} */
+export const locales = ['en','sr-Cyrl','sr']
--- src/locales/main.loader.js
+++ src/locales/main.loader.js
@@ -1,3 +1,6 @@
+// Astro loader template (server-side, synchronous)
+// This is a template file that wuchale will use to generate the actual loader
+
 import { currentRuntime } from 'wuchale/load-utils/server'
 import { loadCatalog, loadCount } from './.wuchale/main.proxy.sync.js'
 
@@ -5,5 +8,8 @@ const key = 'main'
 
 export { key, loadCatalog, loadCount }
 
+// For non-reactive server-side rendering
 export const getRuntime = (loadID = 0) => currentRuntime(key, loadID)
+
+// Same function for compatibility
 export const getRuntimeRx = getRuntime
